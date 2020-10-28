0b4599e990cd414e3010ba45180c4ff07c152dfb
levithan <thegallows@riseup.net>
2020-10-28 14:21:06 -0600

    Normalize tabs

--- 9wm.c
+++ 9wm.c
@@ -61,7 +61,7 @@ Atom _9wm_running;
 Atom _9wm_hold_mode;
 
 char *fontlist[] = {
-        "-*-dejavu sans-bold-r-*-*-14-*-*-*-m-*-*-*",
+	"-*-dejavu sans-bold-r-*-*-14-*-*-*-m-*-*-*",
 	"-adobe-helvetica-bold-r-*-*-14-*-*-*-p-*-*-*",
 	"lucm.latin1.9",
 	"blit",
@@ -228,7 +228,7 @@ main(int argc, char *argv[])
 			font = XLoadQueryFont(dpy, fname);
 			if (font != 0) {
 #ifndef  DEBUG
-                                 fprintf(stderr, "9wm: font '%s' loaded\n", fname);
+				fprintf(stderr, "9wm: font '%s' loaded\n", fname);
 #endif
 				break;
                         }
